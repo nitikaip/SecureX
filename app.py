e8a8a3f40375ab33847609ed67fbe02ca7254813
nitikaip <54106992+nitikaip@users.noreply.github.com>
2019-08-29 21:43:59 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+# encoding: utf-8
+
+import datetime
+import errno
+import os
+import sys
+import tempfile
+import requests
+import meraki
+
+from flask import Flask, request, abort
+
+
+url = 'https://notify-api.line.me/api/notify'
+token = 'WtqN6srzyc7SyGNSLDhlTlUJqrAhDiUfUYaUnJQ6CrW'
+headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
+
+msg = 'Meraki LINE Notify'
+r = requests.post(url, headers=headers, data = {'message':msg})
+
+print (r.text)
+#return (text)
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0',port=os.environ['PORT'])
+

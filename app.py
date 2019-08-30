5055c68de266b5e77ee903c80b8d7ce4da3960b0
nitikaip <54106992+nitikaip@users.noreply.github.com>
2019-08-30 08:46:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def Notify():
 def NotifyPost():
     msg = 'Meraki LINE Notify with POST'   
     r = requests.post(url, headers=headers, data = {'message':msg})
-    print(str(r))
+    print request.form
     return(msg)
     
 if __name__ == "__main__":
